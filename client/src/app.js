9670325787f45a4913af96a157ca2bed4941eceb
Gaz <39915542+gazmac123@users.noreply.github.com>
2018-09-25 09:51:12 +0100

    current view added to handle view state

--- client/src/app.js
+++ client/src/app.js
@@ -1,26 +1,21 @@
 const Calculator = require('./models/Calculator');
 const Journeys = require('./models/journeys');
-const JourneysAllView = require('./views/journeys_all_view');
-const JourneysFormView = require('./views/journeys_form_view');
-const JourneysImpactView = require('./views/journeys_impact_view');
+const CurrentView = require('./views/current_view')
 
 document.addEventListener ('DOMContentLoaded', () => {
+  
   console.log('js loaded')
 
   const journeys = new Journeys();
   journeys.bindEvents();
   journeys.getData();
-  
-  // const viewRenderElement = document.querySelector('#render-view');
-  // const journeysFormView = new JourneysFormView(viewRenderElement);
-  // journeysFormView.bindEvents();
+
+  const navElement = document.querySelectorAll('li');
+  const currentView = new CurrentView(1, navElement);
+  currentView.render();
+  currentView.bindEvents();
 
   const calclculator = new Calculator();
   calclculator.bindEvents();
 
-  const allRenderElement = document.querySelector('#render-view')
-  const journeysAllView = new JourneysAllView(allRenderElement);
-  journeysAllView.bindEvents();
-
-
-}) 
+}); 
--- client/src/views/journeys_all_view.js
+++ client/src/views/journeys_all_view.js
@@ -1,27 +1,28 @@
 const PubSub = require('../helpers/pub_sub.js');
 
-const JourneysAllView = function(renderElement) {
-  this.renderElement = renderElement;
+const JourneysAllView = function() {
+  this.allJourneyData = null;
 }
 
-JourneysAllView.prototype.bindEvents = function () {
+JourneysAllView.prototype.renderFormView = function(){
   PubSub.subscribe('Journeys:data-loaded', (allJourneyData) => {
+    
     console.log(`journeys all view received data: ${allJourneyData}`)
-    this.renderFormView(allJourneyData.detail);
-  });
+    this.allJourneyData = allJourneyData.detail;
+    const renderElement = document.querySelector('#render-view')
+    const newList = document.createElement('ul');
+    
+    this.allJourneyData.forEach(journey => {
+      const distance = journey.distance;
+      const vehicleType = journey.vehicleType;
+      const fuelType = journey.fuelType;
+      const listElement = this.createListElement(distance, vehicleType, fuelType);
+      newList.appendChild(listElement)
+    })
+    renderElement.appendChild(newList)
+  })
+ 
 };
-
-JourneysAllView.prototype.renderFormView = function(allJourneyData){
-  const newList = document.createElement('ul');
-  allJourneyData.forEach(journey => {
-    const distance = journey.distance;
-    const vehicleType = journey.vehicleType;
-    const fuelType = journey.fuelType;
-    const listElement = this.createListElement(distance, vehicleType, fuelType);
-    newList.appendChild(listElement)
-  });
-  this.renderElement.appendChild(newList);
-}
 
 JourneysAllView.prototype.createListElement = function(distance, vehicleType, fuelType){
   const newListElement = document.createElement('li');
@@ -30,7 +31,7 @@ JourneysAllView.prototype.createListElement = function(distance, vehicleType, fu
 }
 
 JourneysAllView.prototype.handleSubmit = function (event) {
-  // event.preventDefault();
+  event.preventDefault();
   const main = document.querySelector('#add-journey');
   
   const header = document.createElement('h3');
