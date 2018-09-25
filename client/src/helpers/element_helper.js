a0fed8458aba6b7970572ddba61f7f8645f7ca58
Euan <thecableton@gmail.com>
2018-09-25 11:03:18 +0100

    refreshing develop branch so working branch is up to date

---
+++ client/src/helpers/element_helper.js
@@ -0,0 +1,6 @@
+const elementHelper = function(tagName, attributes){ // This would be called like this: const newListElement = elementHelper('li', {'class': 'card'});
+  const element = document.createElement(tagName);
+  for(const attrib in attributes) // not sure if const is okay
+    element.setAttribute(attrib, attributes[attrib]);
+  return element;
+}
--- client/src/views/journeys_all_view.js
+++ client/src/views/journeys_all_view.js
@@ -1,4 +1,5 @@
 const PubSub = require('../helpers/pub_sub.js');
+const elementHelper = require('../helpers/element_helper.js');
 
 const JourneysAllView = function() {
   this.allJourneyData = null;
@@ -24,13 +25,6 @@ JourneysAllView.prototype.renderFormView = function(){
  
 };
 
-const elementHelper = function(tagName, attributes){ // This would be called like this: const newListElement = elementHelper('li', {'class': 'card'});
-  const element = document.createElement(tagName);
-  for(const attrib in attributes) // not sure if const okay
-    element.setAttribute(attrib, attributes[attrib]);
-  return element;
-}
-
 JourneysAllView.prototype.renderFormView = function(allJourneyData){
   const newList = document.createElement('ul');
   newList.setAttribute('class', 'cards');
@@ -44,12 +38,12 @@ JourneysAllView.prototype.renderFormView = function(allJourneyData){
   this.renderElement.appendChild(newList);
 }
 
-// JourneysAllView.prototype.createListElement = function(distance, vehicleType, fuelType){
-//   const newListElement = document.createElement('li');
-//   newListElement.setAttribute('class', 'card');
-//   newListElement.innerHTML = `Distance: ${distance} <br/> Vehicle Type: ${vehicleType} <br/> Fuel Type: ${fuelType}`
-//   return newListElement;
-// }
+JourneysAllView.prototype.createListElement = function(distance, vehicleType, fuelType){
+  const newListElement = document.createElement('li');
+  newListElement.setAttribute('class', 'card');
+  newListElement.innerHTML = `Distance: ${distance} <br/> Vehicle Type: ${vehicleType} <br/> Fuel Type: ${fuelType}`
+  return newListElement;
+}
 
 JourneysAllView.prototype.handleSubmit = function (event) {
   event.preventDefault();
