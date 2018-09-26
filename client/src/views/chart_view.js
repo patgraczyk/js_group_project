21045b1171b39582f7005eb35d28fecff63cbd84
Gaz <39915542+gazmac123@users.noreply.github.com>
2018-09-26 16:03:59 +0100

    more charts added, yay

--- client/src/views/chart_view.js
+++ client/src/views/chart_view.js
@@ -5,6 +5,10 @@ const ChartView = function(){
   this.summaryContainer = null;
   this.totalEmissions = null;
   this.totalDistance = null;
+  this.projectionYear = null;
+  this.projectionTenYear = null;
+  this.distanceYear = null;
+  this.distanceTenYear = null;
 }
 
 ChartView.prototype.bindEvents = function(){
@@ -43,9 +47,9 @@ ChartView.prototype.renderAllCharts = function(){
 
   this.renderEmissionsChart(this.totalEmissions); 
   this.renderDistanceChart(this.totalDistance);
-  // this.renderProjectionsChart();
-  // this.renderProjectionsDistanceChart();
-  // this.renderFuelPieChart();
+  this.renderProjectionsChart(this.projectionYear, this.projectionTenYear);
+  this.renderProjectionsDistance(this.distanceYear, this.distanceTenYear);
+  this.renderFuelPieChart();
 
 };
 
@@ -117,182 +121,191 @@ ChartView.prototype.renderDistanceChart = function(totalDistance){
 
 
 
-// // PROJECTIONS CHART EMISSIONS
-// ChartView.prototype.renderProjectionsChart = function(projectionYear, projectionTenYear){
-// Highcharts.chart('container', {
-//     chart: {
-//       type: 'bar'
-//     },
-//     title: {
-//       text: 'Emissions Projections'
-//     },
-//     subtitle: {
-//       text: 'See how your emissions will look over 1 and 10 years'
-//     },
-//     xAxis: {
-//       categories: ['1 year ', '10 years'],
-//       title: {
-//         text: null
-//       }
-//     },
-//     yAxis: {
-//       min: 0,
-//       title: {
-//         text: 'Tonnes CO2e',
-//         align: 'high'
-//       },
-//       labels: {
-//         overflow: 'justify'
-//       }
-//     },
-//     tooltip: {
-//       valueSuffix: ' tonnes CO2e'
-//     },
-//     plotOptions: {
-//       bar: {
-//         dataLabels: {
-//           enabled: true
-//         }
-//       }
-//     },
-//     legend: {
-//       layout: 'vertical',
-//       align: 'right',
-//       verticalAlign: 'top',
-//       x: -40,
-//       y: 80,
-//       floating: true,
-//       borderWidth: 1,
-//       backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-//       shadow: true
-//     },
-//     credits: {
-//       enabled: false
-//     },
-//     series: [{
-//       name: 'One year',
-//       data: [projectionYear]
-//     }, {
-//       name: 'Ten years',
-//       data: [projectionTenYear]
-//     },]
-//   });
+// PROJECTIONS CHART EMISSIONS
+ChartView.prototype.renderProjectionsChart = function(projectionYear, projectionTenYear){
+  const renderProjectionsChart = document.createElement('div');
+  renderProjectionsChart.id = "render_projections_chart";
+  this.container.appendChild(renderProjectionsChart);
+  Highcharts.chart(renderProjectionsChart, {
+      chart: {
+        type: 'bar'
+      },
+      title: {
+        text: 'Emissions Projections'
+      },
+      subtitle: {
+        text: 'See how your emissions will look over 1 and 10 years'
+      },
+      xAxis: {
+        categories: ['Projections'],
+        title: {
+          text: null
+        }
+      },
+      yAxis: {
+        min: 0,
+        title: {
+          text: 'Tonnes CO2e',
+          align: 'high'
+        },
+        labels: {
+          overflow: 'justify'
+        }
+      },
+      tooltip: {
+        valueSuffix: ' tonnes CO2e'
+      },
+      plotOptions: {
+        bar: {
+          dataLabels: {
+            enabled: true
+          }
+        }
+      },
+      legend: {
+        layout: 'vertical',
+        align: 'right',
+        verticalAlign: 'top',
+        x: -40,
+        y: 80,
+        floating: true,
+        borderWidth: 1,
+        backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+        shadow: true
+      },
+      credits: {
+        enabled: false
+      },
+      series: [{
+        name: 'One year',
+        data: [projectionYear]
+      }, {
+        name: 'Ten years',
+        data: [projectionTenYear]
+      },]
+    });
 
-// }
+}
 
 
-// // PROJECTIONS CHART DISTANCE
-// ChartView.prototype.renderProjectionsDistanceChart = function(distanceYear, distanceTenYear){
-//     Highcharts.chart('container', {
-//         chart: {
-//           type: 'bar'
-//         },
-//         title: {
-//           text: 'Emissions Projections'
-//         },
-//         subtitle: {
-//           text: 'See how your emissions will look over 1 and 10 years'
-//         },
-//         xAxis: {
-//           categories: ['1 year ', '10 years'],
-//           title: {
-//             text: null
-//           }
-//         },
-//         yAxis: {
-//           min: 0,
-//           title: {
-//             text: 'Tonnes CO2e',
-//             align: 'high'
-//           },
-//           labels: {
-//             overflow: 'justify'
-//           }
-//         },
-//         tooltip: {
-//           valueSuffix: ' tonnes CO2e'
-//         },
-//         plotOptions: {
-//           bar: {
-//             dataLabels: {
-//               enabled: true
-//             }
-//           }
-//         },
-//         legend: {
-//           layout: 'vertical',
-//           align: 'right',
-//           verticalAlign: 'top',
-//           x: -40,
-//           y: 80,
-//           floating: true,
-//           borderWidth: 1,
-//           backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
-//           shadow: true
-//         },
-//         credits: {
-//           enabled: false
-//         },
-//         series: [{
-//           name: 'One year',
-//           data: [distanceYear]
-//         }, {
-//           name: 'Ten years',
-//           data: [distanceTenYear]
-//         }]
-//       });
+// PROJECTIONS CHART DISTANCE
+ChartView.prototype.renderProjectionsDistance = function(distanceYear, distanceTenYear){
+  const renderProjectionsDistance = document.createElement('div');
+  renderProjectionsDistance.id = "render_projections_distance";
+  this.container.appendChild(renderProjectionsDistance);
+    Highcharts.chart(renderProjectionsDistance, {
+        chart: {
+          type: 'bar'
+        },
+        title: {
+          text: 'Distance Projections'
+        },
+        subtitle: {
+          text: 'See how your distance projections will look over 1 and 10 years'
+        },
+        xAxis: {
+          categories: ['Projections'],
+          title: {
+            text: null
+          }
+        },
+        yAxis: {
+          min: 0,
+          title: {
+            text: 'km',
+            align: 'high'
+          },
+          labels: {
+            overflow: 'justify'
+          }
+        },
+        tooltip: {
+          valueSuffix: 'km'
+        },
+        plotOptions: {
+          bar: {
+            dataLabels: {
+              enabled: true
+            }
+          }
+        },
+        legend: {
+          layout: 'vertical',
+          align: 'right',
+          verticalAlign: 'top',
+          x: -40,
+          y: 80,
+          floating: true,
+          borderWidth: 1,
+          backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+          shadow: true
+        },
+        credits: {
+          enabled: false
+        },
+        series: [{
+          name: 'One year',
+          data: [distanceYear]
+        }, {
+          name: 'Ten years',
+          data: [distanceTenYear]
+        }]
+      });
     
-//     }
+    }
 
-// // PIE CHART SPLIT BY FUEL 
-// ChartView.prototype.renderFuelPieChart = function(){
-//     Highcharts.chart('container', {
-//         chart: {
-//           plotBackgroundColor: null,
-//           plotBorderWidth: null,
-//           plotShadow: false,
-//           type: 'pie'
-//         },
-//         title: {
-//           text: 'Browser market shares in January, 2018'
-//         },
-//         tooltip: {
-//           pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-//         },
-//         plotOptions: {
-//           pie: {
-//             allowPointSelect: true,
-//             cursor: 'pointer',
-//             dataLabels: {
-//               enabled: true,
-//               format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-//               style: {
-//                 color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-//               }
-//             }
-//           }
-//         },
-//         series: [{
-//           name: 'Fuel Type',
-//           colorByPoint: true,
-//           data: [{
-//             name: 'Petrol',
-//             y: 61.41,
-//             sliced: true,
-//             selected: true
-//           }, {
-//             name: 'Diesel',
-//             y: 11.84
-//           }, {
-//             name: 'Hybrid',
-//             y: 10.85
-//           }, {
-//             name: 'Electric',
-//             y: 4.67
-//           }]
-//         }]
-//       });
-//     }
+// PIE CHART SPLIT BY FUEL 
+ChartView.prototype.renderFuelPieChart = function(){
+  const renderFuelPieChart = document.createElement('div');
+  renderFuelPieChart.id = "render_fuel_pie_chart";
+  this.container.appendChild(renderFuelPieChart);
+    Highcharts.chart(renderFuelPieChart, {
+        chart: {
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
+        },
+        title: {
+          text: 'Emissions by fuel type'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: true,
+              format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+              style: {
+                color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+              }
+            }
+          }
+        },
+        series: [{
+          name: 'Fuel Type',
+          colorByPoint: true,
+          data: [{
+            name: 'Petrol',
+            y: 61.41,
+            sliced: true,
+            selected: true
+          }, {
+            name: 'Diesel',
+            y: 11.84
+          }, {
+            name: 'Hybrid',
+            y: 10.85
+          }, {
+            name: 'Electric',
+            y: 4.67
+          }]
+        }]
+      });
+    }
 
 
 
